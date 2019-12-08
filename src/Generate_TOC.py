ab69e8d3ad65fe5717e6d541a98c9ade89672913
EdisonLee <cnljt@outlook.com>
2019-12-08 22:06:25 +0800

    update code for 1. customize Back_to_TOC. 2. fix blank line bugs. 3. adapted to any markdown file with TOC.

--- src/Generate_TOC.py
+++ src/Generate_TOC.py
@@ -3,7 +3,9 @@ from collections import defaultdict
 
 
 def parse_input_file(input_filename, back_to_toc='Back to TOC'):
-    table_of_contents = ['<a class="toc" id="table-of-contents"></a>\n# Table of Contents\n']
+    table_of_contents = [
+        '<a class="toc" id="table-of-contents"></a>\n# Table of Contents\n'
+    ]
     primary_title_count = 0
     output_file = []
     # to judge whether within the triple_quotes ``` ```
@@ -31,19 +33,21 @@ def parse_input_file(input_filename, back_to_toc='Back to TOC'):
                     # dict of secondary title, level 3 title and so on
                     title_dict[pos] += 1
                     index = str(primary_title_count)
-                    for i in range(2, pos+1):
+                    for i in range(2, pos + 1):
                         index += '-' + str(title_dict[i])
 
-                tab = (pos-1) * '\t'
+                tab = (pos - 1) * '\t'
                 title_pos = pos + 1 if line[pos] == ' ' else pos
-                title = line[title_pos:-1] if line[-1] == '\n' else line[title_pos:]
+                title = line[title_pos:
+                             -1] if line[-1] == '\n' else line[title_pos:]
                 table_of_contents.append(f'{tab}+ [{title}](#{index})\n')
                 # add anchor above the header line
                 line_above = f'<a class="toc" id ="{index}"></a>\n'
                 output_file.append(line_above)
                 output_file.append(line)
-                if pos == 1:
-                    output_file.append(f'[{back_to_toc}](#table-of-contents)\n\n')
+                if pos == 1 and back_to_toc:
+                    output_file.append(
+                        f'[{back_to_toc}](#table-of-contents)\n\n')
             else:
                 output_file.append(line)
                 if line.startswith('```'):
@@ -55,39 +59,77 @@ def write_back(output_filename, table_of_contents, output_file, placeholder):
     # for pretty, add two lines
     table_of_contents.append('\n' * 2)
     placeholder += '\n'
-    toc_pos = output_file.index(placeholder) if placeholder in output_file else 0
+    toc_pos = output_file.index(
+        placeholder) if placeholder in output_file else 0
     with open(output_filename, 'w', encoding='utf-8') as f:
         f.writelines(output_file[:toc_pos])
         f.writelines(table_of_contents)
-        f.writelines(output_file[toc_pos+1:])
+        f.writelines(output_file[toc_pos + 1:])
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Generate TOC for your Markdown file.')
-    parser.add_argument('input_filename', metavar='input_filename', type=str,
-                        help='The file that need to generate TOC. You must spacify the input filename.')
-    parser.add_argument('output_filename', metavar='output_filename', type=str, nargs='?',
-                        help='The newly generated file with TOC from the original file.\
-                        If the parameter is omitted, The original file will be overwritten with the newly generated file. NOTE: output_filename=input_filename in default.')
-    parser.add_argument('placeholder', metavar='placeholder', type=str, nargs='?',
-                        default='~~placeholder~~', help='Specify the placeholder, and it will generate TOC at the position of placeholder, otherwise the TOC will generate at the top. NOTE: placeholder="~~placeholder~~" in default.')
+    parser = argparse.ArgumentParser(
+        description='Generate TOC for your Markdown file.')
+    parser.add_argument(
+        'input_filename',
+        metavar='input filename',
+        type=str,
+        help=
+        'The file that need to generate TOC. You must spacify the input filename.'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        metavar='output filename',
+        type=str,
+        nargs='?',
+        help='The newly generated file with TOC from the original file.\
+                        If the parameter is omitted, The original file will be overwritten with the newly generated file. NOTE: output_filename=input_filename in default.'
+    )
+
+    parser.add_argument(
+        '-p',
+        '--placeholder',
+        metavar='placeholder',
+        type=str,
+        nargs='?',
+        default='~~placeholder~~',
+        help=
+        'Specify the placeholder, and it will generate TOC at the position of placeholder, otherwise the TOC will generate at the top. NOTE: placeholder="~~placeholder~~" in default.'
+    )
+
+    parser.add_argument(
+        '-b',
+        '--back_to_toc',
+        metavar='Back to TOC string',
+        type=str,
+        nargs='?',
+        default='Back to TOC',
+        help='Specify the "back to TOC" anchor under the First level heading.')
+
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_args()
     input_filename = args.input_filename
-    output_filename = args.output_filename
+    output_filename = args.output
     placeholder = args.placeholder
+    back_to_toc = args.back_to_toc
     if not output_filename:
-        confirm = input('You did not enter output_filename. Would you like to replace your original file with newly generated file? [Yes/No]\n')
-        if confirm.strip().lower() != 'yes':
+        confirm = input(
+            'You did not enter output_filename. Would you like to cover your original file with newly generated file? [Y/N]\n'
+        )
+        if confirm.strip().lower() != 'y':
             print("### Generated TOC termination.\nThe program has finished.")
             exit()
         else:
             output_filename = input_filename
-    print(f'### Generating markdown toc from {input_filename}, and wtite back to {output_filename}...')
+    print(
+        f'### Generating markdown toc from {input_filename}, and wtite back to {output_filename}...'
+    )
 
-    table_of_contents, output_file = parse_input_file(input_filename)
+    table_of_contents, output_file = parse_input_file(input_filename,
+                                                      back_to_toc)
     write_back(output_filename, table_of_contents, output_file, placeholder)
-    print('### Already generating markdown toc')
+    print(f'### Already generated markdown TOC for {input_filename}.')
